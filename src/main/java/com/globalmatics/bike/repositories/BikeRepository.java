ce91a6ef1bab9930aa2a9634d2c2cd2dcf55ebdd
Freddie01 <Freddie.Williams@morehouse.edu>
2021-04-18 22:16:38 -0400

    Add files via upload

---
+++ src/main/java/com/globalmatics/bike/repositories/BikeRepository.java
@@ -0,0 +1,9 @@
+package com.globalmatics.bike.repositories;
+
+import com.globalmatics.bike.models.Bike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BikeRepository extends JpaRepository<Bike, Long> {
+}
